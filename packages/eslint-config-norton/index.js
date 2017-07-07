a08bc88891eba80f83617bfd5179e3557b82d829
Evan Yamanishi <eyamanishi@wwnorton.com>
2017-07-07 14:08:39 -0400

    node 6 doesn't like function comma-dangles

--- packages/eslint-config-norton/index.js
+++ packages/eslint-config-norton/index.js
@@ -2,6 +2,13 @@
 module.exports = {
     extends: ['airbnb'],
     rules: {
+        'comma-dangle': ['error', {
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+            imports: 'always-multiline',
+            exports: 'always-multiline',
+            functions: 'never',
+        }],
         indent: ['error', 4],
     },
 };
