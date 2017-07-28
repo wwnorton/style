dac7372f2adb42a7aeba0c610574af900e349342
Evan Yamanishi <eyamanishi@wwnorton.com>
2017-07-28 12:01:15 -0400

    use tabs, per style/guide#1

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 /** @see https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb */
 module.exports = {
-    extends: ['airbnb'],
-    rules: {
-        'comma-dangle': ['error', {
-            arrays: 'always-multiline',
-            objects: 'always-multiline',
-            imports: 'always-multiline',
-            exports: 'always-multiline',
-            functions: 'never',             // function trailing commas are only supported in Node 8+
-        }],
-        indent: ['error', 4],
-    },
+	extends: ['airbnb'],
+	rules: {
+		'comma-dangle': ['error', {
+			arrays: 'always-multiline',
+			objects: 'always-multiline',
+			imports: 'always-multiline',
+			exports: 'always-multiline',
+			/**
+             * trailing function commas are only supported in ES2017 (& Node 8+)
+             * @see https://github.com/tc39/proposal-trailing-function-commas
+             */
+			functions: 'never',
+		}],
+		indent: ['error', 'tab'],
+		'no-tabs': 0,
+	},
 };
