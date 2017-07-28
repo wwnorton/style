b007908d96b7357430a4cb9fb236d45c4be6e37d
Evan Yamanishi <eyamanishi@wwnorton.com>
2017-07-28 11:58:10 -0400

    simple tests to root

--- test.js
+++ test.js
@@ -8,13 +8,13 @@ const eslint = require('eslint');
 
 /** Lint all the files in this repo. */
 const repoFiles = [
-    'index.js',
-    'test/index.js',
+	'index.js',
+	'test.js',
 ];
 
 /** Use the rules defined in this repo. */
 const eslintOpts = {
-    useEslintrc: true,
+	useEslintrc: true,
 };
 
 /** Runs the linter on the repo files and asserts no errors were found. */
@@ -22,5 +22,5 @@ const report = new eslint.CLIEngine(eslintOpts).executeOnFiles(repoFiles);
 assert.equal(report.errorCount, 0);
 assert.equal(report.warningCount, 0);
 repoFiles.forEach((file, index) => {
-    assert(report.results[index].filePath.endsWith(file));
+	assert(report.results[index].filePath.endsWith(file));
 });
