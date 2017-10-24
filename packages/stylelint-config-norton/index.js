1c4d29183d3a3faebf30e436a5c5a4b9c3e50ec4
Evan Yamanishi <eyamanishi@wwnorton.com>
2017-10-24 15:48:37 -0400

    add space for scss rules

--- packages/stylelint-config-norton/index.js
+++ packages/stylelint-config-norton/index.js
@@ -3,7 +3,11 @@ module.exports = {
 		'stylelint-config-standard',
 		'stylelint-config-sass-guidelines',
 	],
+	plugins: [
+		'stylelint-scss',
+	],
 	rules: {
+		/** stylelint rules */
 		'at-rule-no-unknown': null,
 		'declaration-property-unit-blacklist': {
 			'font-size': ['px'],
@@ -11,5 +15,8 @@ module.exports = {
 		'function-parentheses-space-inside': 'never-single-line',
 		indentation: 'tab',
 		'max-empty-lines': 2,
+
+		/** stylelint-scss rules */
+		// 'at-else-empty-line-before': 'never',
 	},
 };
