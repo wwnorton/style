2d94a37feb161e48c28114648f746c63d21a2077
Evan Yamanishi <eyamanishi@wwnorton.com>
2017-12-05 14:49:18 -0500

    remove react rules entirely

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ module.exports = {
 	 * use airbnb's well-reasoned style guide as base
 	 * @see https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb
 	 */
-	extends: ['airbnb'],
+	extends: [
+		'eslint-config-airbnb-base',
+		'eslint-config-airbnb-base/rules/strict',
+	],
 	rules: {
 		'comma-dangle': ['error', {
 			arrays: 'always-multiline',
