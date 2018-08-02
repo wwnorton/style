cc85017c40772613cfe75773da71be6cbcef6ab4
Evan Yamanishi <eyamanishi@wwnorton.com>
2018-08-02 10:50:52 -0400

    refactor(test): add comments and finalize testing API

--- packages/commitlint-config-ebook/test.js
+++ packages/commitlint-config-ebook/test.js
@@ -5,26 +5,48 @@ import config from './';
 
 let lint;
 
-test.before(async (t) => {
+/**
+ * Load the config into our lint function
+ * This is run once, before everything else
+ */
+test.before(async () => {
 	const opts = await load(config);
-	lint = str => commitlint(
-		str,
+	/**
+	 * Commitlint function that uses our config
+	 * @param  {String} commitMsg	The commit message
+	 * @return {Object}				The report returned by @commitlint/lint
+	 */
+	lint = commitMsg => commitlint(
+		commitMsg,
 		opts.rules,
 		opts.parserPreset ? { parserOpts: opts.parserPreset.parserOpts } : {}
 	);
 });
 
+/* eslint-disable no-param-reassign */
+/**
+ * Add the lint function to the test context before every test
+ */
 test.beforeEach((t) => {
 	t.context.lint = lint;
 });
 
-const commitTest = (desc, str, fn) => {
-	const title = `${desc}: "${str}"`;
-	test(title, async function (t) {
-		t.context.report = await t.context.lint(str);
+/**
+ * The primary testing interface
+ * @param  {String}   desc      The Ava test description
+ * @param  {String}   commitMsg The commit message to be tested
+ * @param  {Function} fn        Ava's test function
+ */
+const commitTest = (desc, commitMsg, fn) => {
+	const title = `${desc}: "${commitMsg}"`;
+	test(title, async (t) => {
+		t.context.report = await t.context.lint(commitMsg);
 		return fn.apply(this, arguments);
 	});
 };
+/* eslint-enable no-param-reassign */
+
+// BEGIN TESTS
 
 commitTest('invalid type', 'foo: bar', async (t) => {
 	t.false(t.context.report.valid, 'test failure message');
