c37eda489d919400f703b014397200862c4adcaa
Evan Yamanishi <yamanishi1@gmail.com>
2019-09-17 13:03:55 -0400

    chore(lint): lint according to new style

--- packages/stylelint-config-norton/test/helpers/index.js
+++ packages/stylelint-config-norton/test/helpers/index.js
@@ -8,7 +8,7 @@ export const invalidSass = fs.readFileSync(path.resolve('./test/fixtures/invalid
 export const invalidCss = fs.readFileSync(path.resolve('./test/fixtures/invalid.css'), 'utf-8');
 
 export const getWarnings = (res, rule) => Promise.resolve(res)
-	.then(data => data.results[0].warnings.filter(w => w.rule === rule));
+	.then((data) => data.results[0].warnings.filter((w) => w.rule === rule));
 
 export const lint = (code, css = false) => stylelint.lint({
 	code,
