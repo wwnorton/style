f2c3a32fba7523783a46d0d0516059334b293eeb
Evan Yamanishi <yamanishi1@gmail.com>
2020-07-14 14:54:39 -0400

    feat: add overrides for react & typescript

---
+++ overrides/react.js
@@ -0,0 +1,35 @@
+/**
+ * Overrides for usage with React.
+ *
+ * Additional dependencies expected:
+ * - eslint-plugin-jsx-a11y
+ * - eslint-plugin-react
+ * - eslint-plugin-react-hooks
+ *
+ * Note that all of these can be added via `eslint-config-airbnb`.
+ */
+module.exports = {
+	files: ['*.jsx', '*.tsx'],
+	env: { browser: true },
+	plugins: ['react-hooks'],
+	rules: {
+		'react/jsx-indent': ['error', 'tab'],
+		'react/jsx-indent-props': ['error', 'tab'],
+		'react/jsx-filename-extension': [
+			'error',
+			{
+				extensions: ['.jsx', '.tsx'],
+			},
+		],
+		'react/jsx-props-no-spreading': ['off'],
+		'react/static-property-placement': [
+			'error',
+			'property assignment',
+			{
+				defaultProps: 'static public field',
+			},
+		],
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+	},
+};
