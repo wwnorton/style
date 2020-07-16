c6539b40627116b27883bf76aaf2b82317c4265b
Evan Yamanishi <yamanishi1@gmail.com>
2020-07-16 19:17:37 -0400

    test(stylelint): simplify and remove snapshots

--- packages/stylelint-config-norton/test/index.js
+++ packages/stylelint-config-norton/test/index.js
@@ -1,10 +1,15 @@
 import fs from 'fs';
 import test from 'ava';
 import path from 'path';
+import { lint } from 'stylelint';
 import { promisify } from 'util';
-import { lint, getWarnings } from './helpers';
+import sassConfig from '../src/sass';
+import cssConfig from '../src/css';
 
 const readFile = promisify(fs.readFile);
+
+const getWarnings = (res, rule) => Promise.resolve(res)
+	.then((data) => data.results[0].warnings.filter((w) => w.rule === rule));
 
 const rules = [
 	'declaration-property-unit-blacklist',
@@ -15,6 +20,7 @@ const rules = [
 	'scss/at-else-empty-line-before',
 ];
 
+const sass = { rules };
 const css = {
 	rules: [
 		'declaration-property-unit-blacklist',
@@ -25,27 +31,24 @@ const css = {
 	],
 };
 
-const sass = { rules };
-
 test.before(async () => {
-	sass.invalid = await readFile(path.resolve('./test/fixtures/invalid.scss'), 'utf-8');
-	css.invalid = await readFile(path.resolve('./test/fixtures/invalid.css'), 'utf-8');
+	sass.invalid = await readFile(path.join(__dirname, 'fixtures/invalid.scss'), 'utf-8');
+	css.invalid = await readFile(path.join(__dirname, 'fixtures/invalid.css'), 'utf-8');
 
-	sass.valid = await readFile(path.resolve('./test/fixtures/valid.scss'), 'utf-8');
-	css.valid = await readFile(path.resolve('./test/fixtures/valid.css'), 'utf-8');
+	sass.valid = await readFile(path.join(__dirname, 'fixtures/valid.scss'), 'utf-8');
+	css.valid = await readFile(path.join(__dirname, 'fixtures/valid.css'), 'utf-8');
 
-	sass.invalidResult = await lint(sass.invalid);
-	css.invalidResult = await lint(css.invalid, true);
+	sass.invalidResult = await lint({ code: sass.invalid, config: sassConfig });
+	css.invalidResult = await lint({ code: css.invalid, config: cssConfig });
 
-	sass.validResult = await lint(sass.valid);
-	css.validResult = await lint(css.valid, true);
+	sass.validResult = await lint({ code: sass.valid, config: sassConfig });
+	css.validResult = await lint({ code: css.valid, config: cssConfig });
 });
 
 rules.forEach((rule) => {
 	if (sass.rules.includes(rule)) {
 		test(`${rule}: flagged for invalid sass`, async (t) => {
 			const [warning] = await getWarnings(sass.invalidResult, rule);
-			t.snapshot(warning);
 			t.is(warning.rule, rule);
 		});
 	}
@@ -53,7 +56,6 @@ rules.forEach((rule) => {
 	if (css.rules.includes(rule)) {
 		test(`${rule}: flagged for invalid css`, async (t) => {
 			const [warning] = await getWarnings(css.invalidResult, rule);
-			t.snapshot(warning);
 			t.is(warning.rule, rule);
 		});
 	}
