159e033c9ecb9eb58cf24e79280645f6a6b4bd44
Jonathan Chan <jchan@wwnorton.com>
2020-07-22 15:31:45 -0400

    feat: move @include order below declarations to align with airbnb

--- packages/stylelint-config-norton/src/rules/plugins/order.js
+++ packages/stylelint-config-norton/src/rules/plugins/order.js
@@ -1,7 +1,30 @@
 const primerOrder = require('./property-order');
 
 /** order rules */
+/** make @include go to the bottom order to align with Airbnb guidelines */
 module.exports = {
 	'order/properties-alphabetical-order': null,
 	'order/properties-order': primerOrder,
+	"order/order": [
+        [
+          "custom-properties",
+          "dollar-variables",
+          {
+            "type": "at-rule",
+            "name": "extend"
+		},
+		  "declarations",
+          {
+            "type": "at-rule",
+            "name": "include",
+            "hasBlock": false
+          },
+          {
+            "type": "at-rule",
+            "name": "include",
+            "hasBlock": true
+          },
+          "rules"
+        ]
+      ]
 };
