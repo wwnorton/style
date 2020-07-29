4ded364d569fac7e7e3423c0e89c20a974a4c4c1
Jonathan Chan <jchan@wwnorton.com>
2020-07-29 14:43:23 -0400

    feat: revert and remove warning severity for font-size

--- packages/stylelint-config-norton/src/rules/best-practices.js
+++ packages/stylelint-config-norton/src/rules/best-practices.js
@@ -1,10 +1,6 @@
 module.exports = {
 	/** @see https://gitlab.com/wwnorton/style/stylelint-config-norton/issues/1 */
-	/** changes severity to warning */
-	'declaration-property-unit-blacklist': [{
+	'declaration-property-unit-blacklist': {
 		'font-size': ['px'],
 	},
-	{
-		severity: 'warning',
-	}],
 };
