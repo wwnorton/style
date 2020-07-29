60970474f1c7aca616d4a5971bdc431c3e89ce9b
Jonathan Chan <jchan@wwnorton.com>
2020-07-29 13:16:22 -0400

    feat: incorporate prettier config within index.js

--- packages/eslint-config-norton/index.js
+++ packages/eslint-config-norton/index.js
@@ -5,10 +5,16 @@ module.exports = {
 	 * use airbnb's well-reasoned style guide as base
 	 * @see https://github.com/airbnb/javascript/
 	 */
+	/**
+	 * add eslint-config-prettier to handle formatting conflicts with prettier
+	 * @see https://github.com/prettier/eslint-config-prettier
+	 */
 	extends: [
 		'eslint-config-airbnb',
 		'./rules/style',
 		'./rules/react',
+		'eslint-config-prettier',
+		'eslint-config-prettier/react',
 	].map(require.resolve),
 	/**
 	 * Add overrides for React and TypesScript environments.
