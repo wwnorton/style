24ab0fe4e070f6bc333be9baa4c415aac4b23b77
darian-trama <42662538+darian-trama@users.noreply.github.com>
2020-08-10 10:38:29 -0400

    typescript eslint update, added complete override file (#20)
    * typescript eslint update
    
    * added no-extra-semi rule for ts
    
    Co-authored-by: Darian Trama <dtrama@wwnorton.com>

--- packages/eslint-config-norton/overrides/typescript.js
+++ packages/eslint-config-norton/overrides/typescript.js
@@ -16,10 +16,25 @@
  */
 module.exports = {
 	files: ['*.ts', '*.tsx'],
+	//parser:"@typescript-eslint/parser",
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:import/typescript',
+		'prettier/@typescript-eslint'
+	],
+	plugins: [
+		"@typescript-eslint"
+	],
 	rules: {
 		indent: 'off',
 		'no-tabs': 'off',
 		'@typescript-eslint/indent': ['error', 'tab'],
 		'@typescript-eslint/no-empty-interface': 'off',
+		'no-extra-semi': 'off',
+		'@typescript-eslint/no-extra-semi': 'error',
 	},
+	parserOptions: {
+		project: "./tsconfig.json"
+	}
 };
