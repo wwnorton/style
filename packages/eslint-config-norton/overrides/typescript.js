55a80012465da72287bef4396d3ce6c62393bd36
Darian Trama <dtrama@wwnorton.com>
2020-08-10 01:16:29 -0400

    typescript eslint update

--- packages/eslint-config-norton/overrides/typescript.js
+++ packages/eslint-config-norton/overrides/typescript.js
@@ -16,10 +16,23 @@
  */
 module.exports = {
 	files: ['*.ts', '*.tsx'],
+	//parser:"@typescript-eslint/parser",
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:import/typescript',
+		'prettier/@typescript-eslint'
+	],
+	plugins: [
+		"@typescript-eslint"
+	],
 	rules: {
 		indent: 'off',
 		'no-tabs': 'off',
 		'@typescript-eslint/indent': ['error', 'tab'],
 		'@typescript-eslint/no-empty-interface': 'off',
 	},
+	parserOptions: {
+		project: "./tsconfig.json"
+	}
 };
