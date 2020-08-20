9aa63472a068b0517f5660b000d015fd4cc5ee3a
ParmeJon <jonathansychan@gmail.com>
2020-08-20 19:06:27 +0000

    Deploying to gh-pages from  @ f63d101026581589890c553af7f20425afe744b1 🚀

---
+++ c1510a3a.dba5c489.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{76:function(s){s.exports=JSON.parse('{"guidelines":{"allTagsPath":"/style/blog/tags","slug":"guidelines","name":"guidelines","count":1,"permalink":"/style/blog/tags/guidelines"},"style":{"allTagsPath":"/style/blog/tags","slug":"style","name":"style","count":1,"permalink":"/style/blog/tags/style"}}')}}]);
