f8ff14d80a4e587678bc706899af4ec0f73aca9d
Evan Yamanishi <yamanishi1@gmail.com>
2020-08-28 17:50:31 -0400

    refactor(eslint-config-norton): simplify config comment

--- packages/eslint-config-norton/index.js
+++ packages/eslint-config-norton/index.js
@@ -1,15 +1,12 @@
 const typescript = require('./overrides/typescript');
 const typescriptReact = require('./overrides/typescript-react');
 
+/**
+ * Use Airbnb as base and add Prettier to handle formatting conflicts.
+ * @see https://github.com/airbnb/javascript/
+ * @see https://github.com/prettier/eslint-config-prettier
+ */
 module.exports = {
-	/**
-	 * use airbnb's well-reasoned style guide as base
-	 * @see https://github.com/airbnb/javascript/
-	 */
-	/**
-	 * add eslint-config-prettier to handle formatting conflicts with prettier
-	 * @see https://github.com/prettier/eslint-config-prettier
-	 */
 	extends: [
 		'eslint-config-airbnb',
 		'eslint-config-airbnb/hooks',
