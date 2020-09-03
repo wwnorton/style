bda8892166521f0c4de0fde6d3fb979660354675
Jonathan Chan <jchan@wwnorton.com>
2020-09-03 13:29:54 -0400

    refactor: move typescript rules into rules folder

--- packages/eslint-config-norton/typescript-base.js
+++ packages/eslint-config-norton/typescript-base.js
@@ -2,43 +2,37 @@
  * Overrides for usage with TypeScript.
  */
 module.exports = {
-	extends: [
-		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking',
-		'plugin:import/typescript',
-		'prettier/@typescript-eslint'
-	],
-	rules: {
-		/** Enable tabs with @typescript-eslint/indent instead of eslint/indent. */
-		indent: 'off',
-		'@typescript-eslint/indent': ['error', 'tab'],
-
-		/** Add ts to airbnb's list. */
-		'import/extensions': [
-			'error',
-			'ignorePackages',
-			{
-				js: 'never',
-				mjs: 'never',
-				jsx: 'never',
-				ts: 'never',
+	overrides: [
+		{
+			files: ['*.ts?(x)'],
+			parser: '@typescript-eslint/parser',
+			plugins: ['@typescript-eslint'],
+			extends: [
+				'plugin:@typescript-eslint/recommended',
+				'plugin:@typescript-eslint/recommended-requiring-type-checking',
+				'plugin:import/typescript',
+				'prettier/@typescript-eslint',
+				'./rules/typescript-base'
+			],
+			parserOptions: {
+				project: './tsconfig.json',
 			},
-		],
-	},
-	settings: {
-		'import/extensions': [
-			'.js',
-			'.jsx',
-			'.mjs',
-			'.ts',
-		],
-		'import/resolver': {
-			node: {
-				extensions: ['.js', '.json', '.ts'],
+			settings: {
+				'import/extensions': [
+					'.js',
+					'.jsx',
+					'.mjs',
+					'.ts',
+				],
+				'import/resolver': {
+					node: {
+						extensions: ['.js', '.json', '.ts'],
+					},
+					webpack: {
+						extensions: ['.js', '.json', '.ts'],
+					},
+				},
 			},
-			webpack: {
-				extensions: ['.js', '.json', '.ts'],
-			},
-		},
-	},
+		}
+	]
 };
--- packages/eslint-config-norton/typescript.js
+++ packages/eslint-config-norton/typescript.js
@@ -2,55 +2,30 @@
  * Overrides for usage with TypeScript + React.
  */
 module.exports = {
-	files: ['*.tsx'],
-	extends: [
-		'./typescript-base',
-	].map(require.resolve),
-	rules: {
-		/** Add .tsx to react/jsx-filename-extension. */
-		'react/jsx-filename-extension': [
-			'error',
-			{
-				extensions: ['.jsx', '.tsx'],
-			},
-		],
-		/**
-		 * TypeScript supports public class fields so prefer public fields for static properties.
-		 * https://github.com/airbnb/javascript/blob/a24dc34a4a2748c99006a48e997aa0a06b1d4d94/packages/eslint-config-airbnb/rules/react.js#L487-L490
-		 *
-		*/
-		'react/static-property-placement': [
-			'error',
-			'static public field',
-		],
-		/** Add tsx to the base config. */
-		'import/extensions': [
-			'error',
-			'ignorePackages',
-			{
-				js: 'never',
-				jsx: 'never',
-				mjs: 'never',
-				ts: 'never',
-				tsx: 'never',
-			},
-		],
-	},
-	settings: {
-		'import/extensions': [
-			'.js',
-			'.jsx',
-			'.mjs',
-			'.ts',
-			'.tsx',
-		],
-		'import/resolver': {
-			node: {
-				extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
-			},
-			webpack: {
-				extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+	overrides: [
+		{
+			files: ['*.ts?(x)'],
+			extends: [
+				'./typescript-base',
+				'./rules/typescript-react',
+			].map(require.resolve),
+			settings: {
+				'import/extensions': [
+					'.js',
+					'.jsx',
+					'.mjs',
+					'.ts',
+					'.tsx',
+				],
+				'import/resolver': {
+					node: {
+						extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+					},
+					webpack: {
+						extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+					},
+				},
 			},
 		},
-	},
+	],
 };
