d876102dba161c81fbf1cb903d2972b2a6a1272c
Jonathan Chan <jchan@wwnorton.com>
2020-09-09 13:09:07 -0400

    refactor(eslint-config-norton): add comments to security rules and reorder

--- packages/eslint-config-norton/rules/security.js
+++ packages/eslint-config-norton/rules/security.js
@@ -1,7 +1,11 @@
-/*
+/**
 	Security plugins
 	no-unsanitized: https://github.com/mozilla/eslint-plugin-no-unsanitized
-
+	security: https://github.com/nodesecurity/eslint-plugin-security
+	security-node: https://github.com/gkouziik/eslint-plugin-security-node
+	node: https://github.com/mysticatea/eslint-plugin-node/blob/master/lib/rules/no-extraneous-import.js
+	no-wildcard-postmessage: https://github.com/mozfreddyb/eslint-plugin-no-wildcard-postmessage/
+	prototype-pollution-security-rules: https://github.com/LewisArdern/eslint-plugin-prototype-pollution-security-rules
 */
 
 module.exports = {
@@ -19,8 +23,8 @@ module.exports = {
 		"security-node"
 	],
 	rules: {
-		"security-node/detect-dangerous-redirects"      : 0,
 		"security/detect-non-literal-require"           : 0,
+		"security-node/detect-dangerous-redirects"      : 0,
 		"security-node/detect-non-literal-require-calls": 0,
 		"no-wildcard-postmessage/no-wildcard-postmessage": 1,
 		"prototype-pollution-security-rules/detect-merge"        : 1,
