e3462fe6ba6b2f057ae20a849ecbe5e0072dfeb3
Jonathan Chan <jchan@wwnorton.com>
2020-10-08 13:59:10 -0400

    refactor(eslint-config-norton): move bestPractices avoid default export logic  purely into typescript-base

--- packages/eslint-config-norton/rules/typescript-base.js
+++ packages/eslint-config-norton/rules/typescript-base.js
@@ -1,8 +1,14 @@
 module.exports = {
-	extends: [
-		'./bestPractices'
-	].map(require.resolve),
 	rules: {
+		/**
+		 * Avoid export default to improve TypeScript experience. Applicable in
+		 * JavaScript as well, though less impactful.
+		 * Currently just applied to Typescript overrides.
+		 * For rationale, see https://basarat.gitbook.io/typescript/main-1/defaultisbad
+		 */
+		'import/prefer-default-export': 'off',
+		'import/no-default-export': 'error',
+
 		/** Enable tabs with @typescript-eslint/indent instead of eslint/indent. */
 		indent: 'off',
 		'@typescript-eslint/indent': ['error', 'tab'],
