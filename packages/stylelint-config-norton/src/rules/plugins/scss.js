fb3f9a8f315c791a9c2e1cfbea0d851c8e53b665
Evan Yamanishi <yamanishi1@gmail.com>
2021-12-20 10:54:23 -0500

    refactor: manually merge rules

--- packages/stylelint-config-norton/src/rules/plugins/scss.js
+++ packages/stylelint-config-norton/src/rules/plugins/scss.js
@@ -1,23 +1,23 @@
-const extend = require('lodash.merge');
 const { rules: standard } = require('stylelint-config-standard');
+
+const atRuleEmptyLineBefore = standard['at-rule-empty-line-before'];
 
 /** stylelint-scss rules */
 module.exports = {
-	'at-rule-empty-line-before': extend(
-		standard['at-rule-empty-line-before'],
-		[
-			'always', {
-				ignoreAtRules: [
-					// allow @else to come immediately after @if block
-					'else',
-					// allow the various module/imports to come after one another
-					'forward',
-					'import',
-					'use',
-				],
-			},
-		],
-	),
+	'at-rule-empty-line-before': [
+		atRuleEmptyLineBefore[0],
+		{
+			ignoreAtRules: [
+				// allow @else to come immediately after @if block
+				'else',
+				// allow the various module/imports to come after one another
+				'forward',
+				'import',
+				'use',
+			],
+			...atRuleEmptyLineBefore[1],
+		},
+	],
 	'scss/at-else-empty-line-before': 'never',
 	// add deeper nesting to align with Airbnb css/scss style guide
 	'max-nesting-depth': [
